e7fd53e18bf16da1d53235c832fff3a497c232b9
goswamiajay526 <158549090+goswamiajay526@users.noreply.github.com>
2025-01-22 19:36:51 +0530

    Refactor meta tags in layout and document components to improve SEO and remove redundant Twitter tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" style={{ scrollBehavior: "smooth" }}>
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Dream IT – Consulting Services" />
+        <link rel="icon" href="/favicon.jpg" />
+      </Head>{" "}
       <body>
         <Main />
         <NextScript />
