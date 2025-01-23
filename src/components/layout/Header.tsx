4cdf08afcb4639275c1fbbf76f3721859d666855
goswamiajay526 <158549090+goswamiajay526@users.noreply.github.com>
2025-01-23 10:18:15 +0530

    Refactor header component to use Ant Design Drawer for mobile menu and improve service details layout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,28 +2,36 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import ContactFormModal from "../shared/ContactFormModal";
+import { Drawer } from "antd";
 
 const Header = () => {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isDrawerVisible, setIsDrawerVisible] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
+
   const router = useRouter();
 
-  const toggleMobileMenu = () => setMobileMenuOpen((prev) => !prev);
+  const toggleDrawer = () => setIsDrawerVisible((prev) => !prev);
 
   const services = [
-    { name: "Advanced Data Analytics & Visualization", slug: "advanced-analytics" },
-    { name: "Enterprise Resource Planning Implementation", slug: "erp-implementation" },
-    { name: "Digital Transformation & Process Automation", slug: "digital-transformation" },
+    {
+      name: "Advanced Data Analytics & Visualization",
+      slug: "advanced-analytics",
+    },
+    {
+      name: "Enterprise Resource Planning Implementation",
+      slug: "erp-implementation",
+    },
+    {
+      name: "Digital Transformation & Process Automation",
+      slug: "digital-transformation",
+    },
     { name: "Business-Centric IT Ecosystem", slug: "business-applications" },
     { name: "Digital Marketing", slug: "digital-marketing" },
     { name: "Cloud Data Management", slug: "cloud-data-management" },
   ];
 
-  const handleMouseEnter = () => setShowDropdown(true);
-  const handleMouseLeave = () => setShowDropdown(false);
-
-  const isActive = (path:any) => router.pathname === path;
+  const isActive = (path: any) => router.pathname === path;
 
   return (
     <header className="bg-white shadow-lg header_content">
@@ -36,50 +44,69 @@ const Header = () => {
             alt="Logo"
           />
         </Link>
-        <button onClick={toggleMobileMenu} aria-label="Toggle menu">
+        <button onClick={toggleDrawer} aria-label="Toggle menu">
           <svg
-            className="w-6 h-6 text-gray-500 hover:text-gray-700"
+            className={`w-6 h-6 text-gray-500 hover:text-gray-700 transition-transform duration-300 ${
+              isDrawerVisible ? "rotate-90" : ""
+            }`}
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            ></path>
+            {isDrawerVisible ? (
+               <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            ) : (
+               <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            )}
           </svg>
         </button>
       </div>
-      {isMobileMenuOpen && (
-        <nav
-          className="absolute top-full left-0 w-full bg-white px-4 pb-4 shadow-lg z-50"
-          style={{
-            position: "absolute",
-            top: "100%",
-            left: "0",
-            width: "100%",
-          }}
-        >
-          <ul className="text-center space-y-4">
-            <li>
-              <Link href="/" className="text-black-700 font-bold">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/about-us" className="text-black-700 font-bold">
-                About Us
-              </Link>
-            </li>
-            <li>
-              <p
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                className="flex items-center justify-center w-full text-black-700 font-bold"
-                onClick={() => (window.location.href = "/services")}
+
+      <Drawer
+        title="Menu"
+        placement="right"
+        closable
+        onClose={toggleDrawer}
+        open={isDrawerVisible}
+        width={"80%"}
+      >
+        <ul className="text-left space-y-4">
+          <li>
+            <Link
+              href="/"
+              className={`text-black-700 font-bold ${
+                isActive("/") ? "nav-link-active" : " "
+              }`}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about-us"
+              className={`text-black-700 font-bold ${
+                isActive("/about-us") ? "nav-link-active" : ""
+              }`}
+            >
+              About Us
+            </Link>
+          </li>
+          <li>
+            <div className="flex flex-col">
+              <Link
+                href="/services"
+                className="text-black-700 font-bold flex items-center"
               >
                 Services
                 <svg
@@ -95,52 +122,70 @@ const Header = () => {
                     fill="#212529"
                   />
                 </svg>
-              </p>
-              {showDropdown && (
-                <ul className="absolute z-[1000] left-[10] top-[150px] py-4 px-2 mt-2 w-[300px] bg-white border rounded-[18px] shadow-lg">
-                  {services.map((service) => (
-                    <li
-                      key={service.slug}
-                      className="px-4 py-2 hover:bg-gray-100"
-                    >
-                      <Link href={`/services/${service.slug}`}>
-                        <p className="text-16 font-semibold text-black-700">
-                          {service.name}
-                        </p>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-            <li>
-              <Link href="/industries" className="text-black-700 font-bold">
-                Industries
               </Link>
-            </li>
-            <li>
-              <Link href="/insights" className="text-black-700 font-bold">
-                Insights
-              </Link>
-            </li>
-            <li>
-              <Link href="/career" className="text-black-700 font-bold">
-                Life @DreamIT
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      )}
+              <ul className="mt-2 pl-4">
+                {services.map((service) => (
+                  <li
+                    key={service.slug}
+                    className="flex items-start px-4 py-2 hover:bg-gray-100"
+                  >
+                    <span className="w-2 h-2 bg-blue-600 rounded-full mr-3 flex-shrink-0 mt-2"></span>
+                    <Link href={`/services/${service.slug}`}>
+                      <p className="text-16 font-semibold text-black-700 leading-tight">
+                        {service.name}
+                      </p>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+          <li>
+            <Link
+              href="/industries"
+              className={`text-black-700 font-bold ${
+                isActive("/industries") ? "nav-link-active" : ""
+              }`}
+            >
+              Industries
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/insights"
+              className={`text-black-700 font-bold ${
+                isActive("/insights") ? "nav-link-active" : ""
+              }`}
+            >
+              Insights
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/career"
+              className={`text-black-700 font-bold ${
+                isActive("/career") ? "nav-link-active" : ""
+              }`}
+            >
+              Life @DreamIT
+            </Link>
+          </li>
+        </ul>
+      </Drawer>
 
       {/* Desktop Navbar */}
       <div className="navbar-for-web justify-between items-center container h-16">
         <Link href="/">
-          <img className="lg:w-36 h-auto md:w-28" src="/assets/icons/dreamItLogo.svg" alt="Logo" />
+          <img
+            className="lg:w-36 h-auto md:w-28"
+            src="/assets/icons/dreamItLogo.svg"
+            alt="Logo"
+          />
         </Link>
         <nav className="flex space-x-8">
           <Link
             href="/"
-            className={` ${
+            className={`${
               isActive("/") ? "nav-link-active" : "nav-link-unactive"
             }`}
           >
@@ -148,7 +193,7 @@ const Header = () => {
           </Link>
           <Link
             href="/about-us"
-            className={`  ${
+            className={`${
               isActive("/about-us") ? "nav-link-active" : "nav-link-unactive"
             }`}
           >
@@ -156,14 +201,13 @@ const Header = () => {
           </Link>
           <div
             className="relative group"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
+            onMouseEnter={() => setShowDropdown(true)}
+            onMouseLeave={() => setShowDropdown(false)}
           >
             <button
               className={`flex items-center ${
                 isActive("/services") ? "nav-link-active" : "nav-link-unactive"
               }`}
-              onClick={() => (window.location.href = "/services")}
             >
               Services
               <svg
@@ -183,9 +227,14 @@ const Header = () => {
             {showDropdown && (
               <ul className="absolute z-[1000] left-[-10] top-4 py-4 px-2 mt-2 w-[300px] bg-white border rounded-[18px] shadow-lg">
                 {services.map((service) => (
-                  <li key={service.slug} className="px-4 py-2 hover:bg-gray-100">
+                  <li
+                    key={service.slug}
+                    className="px-4 py-2 hover:bg-gray-100"
+                  >
                     <Link href={`/services/${service.slug}`}>
-                      <p className="text-16 font-semibold text-black-700">{service.name}</p>
+                      <p className="text-16 font-semibold text-black-700">
+                        {service.name}
+                      </p>
                     </Link>
                   </li>
                 ))}
@@ -194,7 +243,7 @@ const Header = () => {
           </div>
           <Link
             href="/industries"
-            className={` ${
+            className={`${
               isActive("/industries") ? "nav-link-active" : "nav-link-unactive"
             }`}
           >
@@ -202,7 +251,7 @@ const Header = () => {
           </Link>
           <Link
             href="/insights"
-            className={` ${
+            className={`${
               isActive("/insights") ? "nav-link-active" : "nav-link-unactive"
             }`}
           >
@@ -210,7 +259,7 @@ const Header = () => {
           </Link>
           <Link
             href="/career"
-            className={` ${
+            className={`${
               isActive("/career") ? "nav-link-active" : "nav-link-unactive"
             }`}
           >
@@ -224,7 +273,10 @@ const Header = () => {
           Get Started
         </button>
       </div>
-      <ContactFormModal isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible} />
+      <ContactFormModal
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+      />
     </header>
   );
 };
