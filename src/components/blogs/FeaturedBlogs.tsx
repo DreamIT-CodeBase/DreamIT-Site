5321d06436dff26c36ccf0118f02315017bc3c4d
goswamiajay526 <158549090+goswamiajay526@users.noreply.github.com>
2025-03-05 11:56:09 +0530

    Refactor FeaturedBlogs component for improved active state handling; utilize router for active link detection and enhance hover effects

--- src/components/blogs/FeaturedBlogs.tsx
+++ src/components/blogs/FeaturedBlogs.tsx
@@ -1,36 +1,44 @@
 import { useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function FeaturedBlogs({ featureBlogsData }: any) {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const router = useRouter();
 
   return (
     <div className="rounded-[15px] bg-white py-6 mt-10 min-h-[200px] max-h-[300px] overflow-y-auto featured-blogs-scrollbar">
       <h2 className="mb-3 text-[20px] ml-6 font-semibold">Featured article</h2>
 
       <div className="space-y-4">
-        {featureBlogsData?.map((article: any, index: any) => (
-          <div
-            key={index}
-            className={`relative pl-4 transition-colors duration-200 ${
-              activeIndex === index
-                ? "border-l-[3.5px] border-[#00A9FF]"
-                : "border-l-2 border-transparent "
-            }`}
-            onMouseEnter={() => setActiveIndex(index)}
-          >
-            <Link
-              href={article.slug}
-              className={`block ${
-                activeIndex === index
-                  ? "text-[#00A9FF]"
-                  : "text-[#1B1B1F] hover:text-primary/80"
-              } xl:text-[18px] lg:text-[18px] md:text-[16px] sm:text-[16px] xs:text-[16px] font-medium`}
+        {featureBlogsData?.map((article: any, index: any) => {
+          const isActive = router.asPath === article.slug || activeIndex === index;
+
+          return (
+            <div
+              key={index}
+              className={`relative pl-4 transition-colors duration-200 ${
+                isActive
+                  ? "border-l-[3.5px] border-[#00A9FF]"
+                  : "border-l-2 border-transparent"
+              }`}
+              onMouseEnter={() => activeIndex === -1 && setActiveIndex(index)}
+              onMouseLeave={() => activeIndex === -1 && setActiveIndex(-1)}
             >
-              {article.title}
-            </Link>
-          </div>
-        ))}
+              <Link
+                href={article.slug}
+                className={`block ${
+                  isActive
+                    ? "text-[#00A9FF]"
+                    : "text-[#1B1B1F] hover:text-primary/80"
+                } xl:text-[18px] lg:text-[18px] md:text-[16px] sm:text-[16px] xs:text-[16px] font-medium`}
+                onClick={() => setActiveIndex(index)}
+              >
+                {article.title}
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
