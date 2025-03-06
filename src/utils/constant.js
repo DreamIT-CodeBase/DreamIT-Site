147eff73d57d0796f25e94710a329c15c6ae9113
goswamiajay526 <158549090+goswamiajay526@users.noreply.github.com>
2025-03-06 15:50:53 +0530

    Update LEAD_API endpoint to new tenant URL for improved data access

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 export const PREVIEW = "preview";
 
 export const AHD_HOST = `https://pagepilot.fabbuilder.com/api/tenant/67beb76503c96859162a68f7`;
-export const LEAD_API = `https://cs.fabbuilder.com/api/tenant/667919ea62ce1937dc2c9b01`;
+export const LEAD_API = `https://cs.fabbuilder.com/api/tenant/67c9764e01facb84729efc1d`;
 
 
