1030c63b3331dd023bb7ef25313b934b650b79f6
Ajay <ajay.goswami@coderower.com>
2025-03-20 21:19:39 +0530

    feat: integrate Google Tag Manager and update SMTP script loading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,16 +4,31 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en" style={{ scrollBehavior: "smooth" }}>
-      <Head/>
-       <body>
-        <Main />
+      <Head>
         <Script
-  src="https://smtpjs.com/v3/smtp.js"
-  strategy="beforeInteractive"
-/>
+          strategy="beforeInteractive"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MQDCHW14ES"
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+             window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-MQDCHW14ES');
+          `}
+        </Script>
+
+        <Script
+          src="https://smtpjs.com/v3/smtp.js"
+          strategy="beforeInteractive"
+        />
+      </Head>
+      <body>
+        <Main />
 
         <NextScript />
-
       </body>
     </Html>
   );
