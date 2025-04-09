db788f63c182424c5559fa6666421f2c72a08e84
Ajay <ajay.goswami@coderower.com>
2025-04-09 20:00:06 +0530

    fix: update sitemap configuration to exclude additional paths

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,10 @@ module.exports = {
     siteUrl: 'https://dreamitcs.com',
     generateRobotsTxt: true,
     changefreq: 'daily',
+    exclude: [
+      '/blogs',
+      '/case-studies/preview',
+      '/blogs/preview',
+      '/case-studies',
+    ],
   }
