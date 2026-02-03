91ccc2e9c6730ed039afd721bbd35c5bd91ecc7a
devTarunJuneja <tarunjuneja471@gmail.com>
2026-02-03 12:12:27 +0530

    Force remove trailing slash URLs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   images: { unoptimized: true },
-
   trailingSlash: false,
-
-  async redirects() {
-    return [
-      {
-        source: "/:path*/",
-        destination: "/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
