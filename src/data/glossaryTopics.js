bbe85d786264bddfa20357604670919af5e94421
devTarunJuneja <tarunjuneja471@gmail.com>
2026-02-09 11:09:46 +0530

    message

--- src/data/glossaryTopics.js
+++ src/data/glossaryTopics.js
@@ -11,7 +11,7 @@ export const glossaryTopics = [
     description:"One Lake is a unified data lake solution that provides a single place to store and manage all your data."
   },
   {
-    slug:"powerbi-desktop-projects",
+    slug:"powerbi-Desktop-projects",
     title:"Power BI Desktop Projects",
     description:"Power BI Desktop Projects are a feature that allows you to work with multiple datasets in a single Power BI Desktop file."
   },
