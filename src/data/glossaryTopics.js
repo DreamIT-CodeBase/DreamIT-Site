65520ea6a8a1d27f044b03ffcde40736da7251e7
devTarunJuneja <tarunjuneja471@gmail.com>
2026-02-09 10:58:26 +0530

    name chnage

--- src/data/glossaryTopics.js
+++ src/data/glossaryTopics.js
@@ -6,12 +6,12 @@ export const glossaryTopics = [
       "Model Context Protocol (MCP) defines conventions for passing context and metadata to LLMs.",
   },
   {
-    slug:"OneLake",
+    slug:"one-lake",
     title:"One Lake",
     description:"One Lake is a unified data lake solution that provides a single place to store and manage all your data."
   },
   {
-    slug:"powerbi-Desktop-projects",
+    slug:"powerbi-desktop-projects",
     title:"Power BI Desktop Projects",
     description:"Power BI Desktop Projects are a feature that allows you to work with multiple datasets in a single Power BI Desktop file."
   },
